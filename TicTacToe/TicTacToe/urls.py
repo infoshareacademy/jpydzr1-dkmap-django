e36f839e18a0b684aef3426d6d94223425ff2b45
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-01-03 08:16:07 +0000

    New Game view created, minor changes

--- TicTacToe/TicTacToe/urls.py
+++ TicTacToe/TicTacToe/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('menu/', include('menu.urls')),
+    path('', include('menu.urls')),
 
 ]
--- TicTacToe/menu/urls.py
+++ TicTacToe/menu/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from menu.views import WelcomeView, GameDetailView
+from menu.views import WelcomeView, GameDetailView, NewGameView
 
 
 urlpatterns = [
     path('', WelcomeView.as_view(), name='welcome-view'),
     path('game-detail/', GameDetailView.as_view(), name='game-detail'),
+    path('new-game/', NewGameView.as_view(), name='new-game-view'),
 
 ]

4de99315998b0893bd3c1839f613401d62066d3d
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-01-10 16:01:00 +0000

    game model added

--- TicTacToe/game/models.py
+++ TicTacToe/game/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
 
+class Game(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    multiplayer_game = models.BooleanField(default=False)
+    win = models.BooleanField(default=False)
+    start_time = models.DateTimeField(auto_now_add=True, blank=True)
+    finish_time = models.DateTimeField()
+    game_duration = models.DurationField(default=0)
+
+    def __str__(self):
+        return f"{self.start_time} - {self.user.username}"

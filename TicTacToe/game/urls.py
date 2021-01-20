7f014fed6cbd39dae131f5b38f2beebcf1728775
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-01-20 21:18:41 +0000

    URLs updated

--- TicTacToe/game/urls.py
+++ TicTacToe/game/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-from game.views import ProfileView, BoardView
+from django.urls import path, include
+from .views import ProfileView, BoardView, ApiView, login, CreateBoard, UpdateBoard
+from rest_framework import routers
 
+
+router = routers.DefaultRouter()
+router.register(r'boards', ApiView, 'board')
 
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile-view'),
     path('board/', BoardView.as_view(), name='board'),
+    path('api/', include(router.urls)),
+    path('api/login', login),
+    path('api/create', CreateBoard.as_view(), name='create_board'),
+    path('api/update', UpdateBoard.as_view(), name='update_board'),
 
 ]

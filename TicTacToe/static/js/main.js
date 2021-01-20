c1692db4c1dab8cd4dd83d6a402e216f95156167
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-01-20 21:20:08 +0000

    AJAX functions added to connect with API

--- TicTacToe/static/js/main.js
+++ TicTacToe/static/js/main.js
@@ -13,3 +13,48 @@ hamburger.addEventListener('click', () => {
 function clicked() {
     return confirm('Message.');
 }
+
+var csrftoken = $.cookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+
+function createBoard() {
+    $.ajax({
+        type: "POST",
+        url: newBoard,
+})}
+
+
+function updateBoard(button_id) {
+    $.ajax({
+        type: "PUT",
+        url: newUpdateBoard,
+        data: {'button_id': button_id},
+        dataType: 'json',
+
+        success: function(data) {
+            document.querySelector('#' + button_id).innerHTML = data[button_id]
+          },
+        error: function(data) {
+        alert(`das`)
+        }
+})}
+
+
+$('[id*="_field"]').click(function () {
+    let button_id = this.id
+    updateBoard(button_id)
+});
+

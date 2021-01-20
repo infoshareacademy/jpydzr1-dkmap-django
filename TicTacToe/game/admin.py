37618bb5e74eb6f5921c5b22faf6193171cdeb73
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-01-20 21:16:35 +0000

    New model registered

--- TicTacToe/game/admin.py
+++ TicTacToe/game/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from game.models import Game
+from game.models import Game, Board
+
 
 admin.site.register(Game)
+admin.site.register(Board)
+
 
 
--- TicTacToe/menu/admin.py
+++ TicTacToe/menu/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from menu.models import PlayerStatistic
 
+admin.site.site_header = 'TicTacToe'
+
 admin.site.register(PlayerStatistic)
 
 

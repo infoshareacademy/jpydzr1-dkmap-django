f89a3fdcb550fdf106294408f73adf61768fd12d
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-05 20:11:32 +0000

    URLs updated

--- TicTacToe/game/urls.py
+++ TicTacToe/game/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
-from .views import ProfileView, BoardView, ApiView, login, CreateBoard, UpdateBoard, JoinBoard, RefreshBoard
+from .views import ProfileView, \
+    JoinGameBoardView, ApiView, login, \
+    CreateBoard, UpdateBoard, JoinBoard, \
+    RefreshBoard, ListBoardView
 from rest_framework import routers
 
 
@@ -8,12 +11,13 @@ router.register(r'boards', ApiView, 'board')
 
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile-view'),
-    path('board/', BoardView.as_view(), name='board'),
+    path('boards/', ListBoardView.as_view(), name='list-board'),
+    path('boards/<int:pk>/', JoinGameBoardView.as_view(), name='game-board'),
+
     path('api/', include(router.urls)),
     path('api/login', login),
     path('api/create', CreateBoard.as_view(), name='create_board'),
     path('api/update', UpdateBoard.as_view(), name='update_board'),
     path('api/join', JoinBoard.as_view(), name='join_board'),
-    path('api/refresh', RefreshBoard.as_view(), name='refresh_board'),
-
+    path('api/refresh', RefreshBoard.as_view(), name='refresh_board')
 ]
--- TicTacToe/menu/urls.py
+++ TicTacToe/menu/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from menu.views import WelcomeView, GameDetailView, NewGameView
+from .views import WelcomeView, GameDetailView, NewGameView
 
 
 urlpatterns = [
     path('', WelcomeView.as_view(), name='welcome-view'),
     path('game-detail/', GameDetailView.as_view(), name='game-detail'),
-    path('new-game/', NewGameView.as_view(), name='new-game-view'),
+    path('new-game/', NewGameView.as_view(), name='new-game-view')
 
 ]

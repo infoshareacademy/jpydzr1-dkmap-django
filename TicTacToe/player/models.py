fa01fb3205ab0e88d4f6cc7e145a0eb94305dfc5
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-11 19:45:04 +0000

    PlayerStats created

--- TicTacToe/player/models.py
+++ TicTacToe/player/models.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.models import AbstractUser, Group
+from django.db import IntegrityError
+from menu.models import PlayerStatistic
+import logging
+
+db_logger = logging.getLogger('db')
 
 
 class CustomUser(AbstractUser):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         group = Group.objects.get(name='Users')
+
+        try:
+            PlayerStatistic.objects.create(user=self)
+        except IntegrityError as e:
+            db_logger.error("duplicate key value violates unique constraint", exc_info=True)
+
         self.groups.add(group)

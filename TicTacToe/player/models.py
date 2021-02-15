8420d0ede17eab3d754c065c82737ef37af3657d
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-15 11:48:55 +0000

    default permission changed, default group bug fixed

--- TicTacToe/player/models.py
+++ TicTacToe/player/models.py
@@ -7,13 +7,13 @@ db_logger = logging.getLogger('db')
 
 
 class CustomUser(AbstractUser):
+
+    class Meta:
+        default_permissions = ()
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        group = Group.objects.get(name='Users')
-
         try:
             PlayerStatistic.objects.get_or_create(user=self)
         except IntegrityError as e:
-            db_logger.error("duplicate key value violates unique constraint", exc_info=True)
-
-        self.groups.add(group)
+            db_logger.error("Duplicate key value violates unique constraint", exc_info=True)

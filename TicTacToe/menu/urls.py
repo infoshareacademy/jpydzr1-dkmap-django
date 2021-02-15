8a65a6414c564790c21c3d17201e719bc0386066
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-15 14:58:31 +0000

    Logging-view added and logs-report create

--- TicTacToe/menu/urls.py
+++ TicTacToe/menu/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import WelcomeView, GameDetailView, NewGameView
+from .views import WelcomeView, GameDetailView, NewGameView, LoggingReportView, export_log_to_csv
+
+app_name = 'menu'
 
 
 urlpatterns = [
     path('', WelcomeView.as_view(), name='welcome-view'),
     path('game-detail/', GameDetailView.as_view(), name='game-detail'),
-    path('new-game/', NewGameView.as_view(), name='new-game-view')
+    path('new-game/', NewGameView.as_view(), name='new-game-view'),
+    path('log-report/', LoggingReportView.as_view(), name='log-report-view'),
+    path('export-to-csv', export_log_to_csv, name='export-to-csv')
 
 ]

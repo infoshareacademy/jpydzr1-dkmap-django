6ab5ba9fbf4236cc0aa1a52e4f8210e32a534a8b
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-15 12:06:54 +0000

    Fields change to PositiveIntegerField

--- TicTacToe/menu/models.py
+++ TicTacToe/menu/models.py
@@ -7,9 +7,9 @@ class PlayerStatistic(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     time_spend_in_game = models.DurationField(default=timedelta(seconds=0))
     best_game_time = models.DurationField(default=timedelta(seconds=0))
-    game_counter = models.IntegerField(default=0)
-    win_counter = models.IntegerField(default=0)
-    lose_counter = models.IntegerField(default=0)
+    game_counter = models.PositiveIntegerField(default=0)
+    win_counter = models.PositiveIntegerField(default=0)
+    lose_counter = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         if self.user:

b45ae72f8b4a843db574aac6917ab0eb27e92856
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-19 08:56:33 +0000

    Serializer for PlayerStatistic model created

--- TicTacToe/player/models.py
+++ TicTacToe/player/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser, Group
 from django.db import IntegrityError
-from menu.models import PlayerStatistic
+from stats.models import PlayerStatistic
 import logging
 
 db_logger = logging.getLogger('db')
--- TicTacToe/reports/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from django_db_logger.models import StatusLog
-
-
-class LoggerSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = StatusLog
-        fields = '__all__'

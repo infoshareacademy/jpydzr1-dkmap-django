6900a15ab825a9e36239558e44e3b66cc1ae6466
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-19 08:54:30 +0000

    PlayerStatistics model relocated

--- TicTacToe/menu/models.py
+++ TicTacToe/menu/models.py
@@ -1,27 +1,2 @@
 from django.db import models
-from django.conf import settings
-from datetime import timedelta
 
-
-class PlayerStatistic(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    time_spend_in_game = models.DurationField(default=timedelta(seconds=0))
-    best_game_time = models.DurationField(default=timedelta(seconds=0))
-    game_counter = models.PositiveIntegerField(default=0)
-    win_counter = models.PositiveIntegerField(default=0)
-    lose_counter = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        if self.user:
-            return f"Statistics: {self.user.username}"
-        return 'Account does not exist.'
-
-    def add_win(self) -> int:
-        self.win_counter += 1
-        self.save()
-        return self.win_counter
-
-    def add_game(self) -> int:
-        self.game_counter += 1
-        self.save()
-        return self.game_counter

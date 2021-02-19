febff77cf4e066c580e048b9cf51338d90baaa96
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-19 08:55:52 +0000

    get_game_time method added, change model field type

--- TicTacToe/menu/admin.py
+++ TicTacToe/menu/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import PlayerStatistic
 
 admin.site.site_header = 'TicTacToe'
-
-
-@admin.register(PlayerStatistic)
-class HouseAdmin(admin.ModelAdmin):
-    pass
-

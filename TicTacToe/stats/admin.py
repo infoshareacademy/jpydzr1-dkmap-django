fdec76b2edbd35a1b80a5229fa74c92fe42ea0e9
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-21 11:49:31 +0000

    New action - pdf report

--- TicTacToe/stats/admin.py
+++ TicTacToe/stats/admin.py
@@ -1,7 +1,35 @@
+import pdfkit
 from django.contrib import admin
+from django.http import HttpResponse
+from django.template.loader import get_template
+
 from .models import PlayerStatistic
+
+
+def get_a_report(modeladmin, request, queryset):
+
+    template = get_template('pdf-report.html')
+    html = template.render({
+        'queryset': queryset,
+        'labels': ['gdansk', 'moskwa'],
+        'data': [1231312, 3213123]
+    })
+
+    options = {
+        'page-size': 'Letter',
+        'encoding': "UTF-8",
+    }
+    pdf = pdfkit.from_string(html, False, options)
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment'
+    filename = "person_list_pdf.pdf"
+    return response
+
+
+get_a_report.short_description = 'Select statistic and download a report.'
 
 
 @admin.register(PlayerStatistic)
 class PlayerStatsAdmin(admin.ModelAdmin):
-    pass
+    ordering = ['-game_counter']
+    actions = [get_a_report]
--- TicTacToe/stats/urls.py
+++ TicTacToe/stats/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import StatsApi
+from django_pdfkit import PDFView
 
 app_name = 'stats'
 router = routers.DefaultRouter()
@@ -9,5 +10,6 @@ router.register(r'stats', StatsApi, 'stats')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path(r'report-pdf/', PDFView.as_view(template_name='pdf-report.html'), name='report-pdf'),
 
 ]
